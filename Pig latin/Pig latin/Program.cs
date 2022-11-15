2b9429630451f6d900ab9d92974c1b3ca62a00cb
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-15 09:26:31 +0100

    upload

---
+++ Pig latin/Pig latin/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Pig_latin;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter word or sentence");
+        var words = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(words))
+        {
+            //make all the letters lowercase
+            words = words?.ToLower();
+
+            pigLatin.con2latin(words);
+        }
+        else
+        {
+            Console.WriteLine("Empty: Enter a valid word");
+        }
+
+       
+    }
+}
+
