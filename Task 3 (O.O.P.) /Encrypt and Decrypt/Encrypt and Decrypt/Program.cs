465b79c73ccfe12c8b7b8c11a7808b42af324e17
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-15 12:57:23 +0100

    upload

--- Task 3 (O.O.P.) /Encrypt and Decrypt/Encrypt and Decrypt/Program.cs
+++ Task 3 (O.O.P.) /Encrypt and Decrypt/Encrypt and Decrypt/Program.cs
@@ -6,21 +6,25 @@ class Program
         Console.WriteLine("what do you wanty to do (Encrypt or Decrypt): ");
         var input = Console.ReadLine();
         
+        
 
 
         if (input?.ToUpper() == "ENCRYPT")
         {
             Console.WriteLine("what do you want to Encrypt: ");
             var inputEncrypt = Console.ReadLine();
+            string inputEncrypt1 = inputEncrypt.Trim();
 
-            string output = Encode_and_decode.encrypt1(inputEncrypt);
+            string output = Encode_and_decode.encrypt1(inputEncrypt1);
             Console.WriteLine(output);
 
         }else if (input?.ToUpper() == "DECRYPT")
         {
             Console.WriteLine("what do you want to Decrypt: ");
             var inputDecrypt = Console.ReadLine();
-            string output = Encode_and_decode.decrypt1(inputDecrypt);
+            string inputDecrypt1 = inputDecrypt.Trim();
+
+            string output = Encode_and_decode.decrypt1(inputDecrypt1);
             Console.WriteLine(output);
         }
         else
