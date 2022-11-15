67d706b60820d03e53570ae6eae861d8a7a58343
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-15 09:38:39 +0100

    upload

---
+++ Next four leap year/Next four leap year/Leapyear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Next_four_leap_year
+{
+	public class Leapyear
+	{
+        public int count;
+        public int leap_year;
+        public int totalyears;
+        public Leapyear()
+		{
+            count = 4;
+            leap_year = 2020;
+            totalyears = 20;
+        }
+        public static void NextLeapYears()
+        {
+            var Leapyear = new Leapyear();
+            int count = Leapyear.count;
+            int totalyears = Leapyear.totalyears;
+            int leapYear = Leapyear.leap_year;
+            while (count < totalyears)
+            {
+                //since leap years are always after 4yrs or can be divided by 4
+                leapYear = leapYear + 4;
+
+                Console.WriteLine(leapYear);
+                count += 4;
+            }
+        }
+	}
+}
+
---
+++ Next four leap year/Next four leap year/Program.cs~
@@ -0,0 +1,9 @@
+﻿namespace Next_four_leap_year;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Leapyear.NextLeapYears();
+    }
+}
+
